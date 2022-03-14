5cc52cac3c744e74c046c3ab355c81126d455a55
Ben Tolliday <ben.tolliday@dreamlax.net>
2022-03-14 16:49:12 +1030

    Release 1.0.0

---
+++ Queue.lua
@@ -0,0 +1,29 @@
+queue = {}
+
+function queue.push(self, item)
+	table.insert(self.list, item)
+end
+
+function queue.pop(self)
+	return table.remove(self.list, 1)
+end
+
+function queue.is_empty(self)
+	return #self.list == 0
+end
+
+function queue.len(self)
+	return #self.list
+end
+
+function queue.new()
+	return {
+		list = {},
+		push = queue.push,
+		pop = queue.pop,
+		is_empty = queue.is_empty,
+		len = queue.len,
+	}
+end
+
+return queue
---
+++ TimeToLive.lua
@@ -0,0 +1,115 @@
+local startDrag = function(frame) if IsAltKeyDown() then frame:StartMoving() end end
+local stopDrag = function(frame) frame:StopMovingOrSizing() end
+
+local UnitHealth, UnitHealthMax = UnitHealth, UnitHealthMax
+
+TTLTargetBar, TTLFocusBar = ...
+
+local previousHealthTicks = queue.new()
+
+local function addHealthTick(hp)
+	previousHealthTicks:push(hp)
+
+	if (previousHealthTicks:len() > 5) then
+		previousHealthTicks:pop();
+	end
+end
+
+local function getDpsAverage()
+	local totalDamage = 0;
+	totalDamage = totalDamage + (previousHealthTicks.list[5] - previousHealthTicks.list[4])
+	totalDamage = totalDamage + (previousHealthTicks.list[4] - previousHealthTicks.list[3])
+	totalDamage = totalDamage + (previousHealthTicks.list[3] - previousHealthTicks.list[2])
+	totalDamage = totalDamage + (previousHealthTicks.list[2] - previousHealthTicks.list[1])
+	if (totalDamage >= 0) then
+		return 0
+	end
+
+	local averageDamage = totalDamage / 5
+	return -1 * averageDamage
+
+end
+
+local function showBlank()
+	TTLFocusBar:SetText("")
+	TTLTargetBar:SetText("")
+end
+
+C_Timer.NewTicker(1, function()
+	if (UnitAffectingCombat("player") == false) then
+		return
+	end
+	-- Add current health of enemy to previous tickss
+	local hp = UnitHealth("target")
+	if (hp == 0) then
+		showBlank()
+		return;
+	end
+	addHealthTick(hp)
+
+	if (previousHealthTicks:len() == 5) then
+		local dpsAverage = getDpsAverage();
+		if (dpsAverage == 0) then
+			showBlank()
+		end
+		local secondsToKill = hp / dpsAverage;
+		if (secondsToKill > 3600) then
+			showBlank()
+		else
+			local minutes = secondsToKill / 60;
+			local seconds = secondsToKill - (math.floor(minutes) * 60);
+			if (math.floor(minutes) > 0) then
+				TTLFocusBar:SetFormattedText("%1dm %1ds", minutes, seconds)
+				TTLTargetBar:SetFormattedText("%1dm %1ds", minutes, seconds)
+			else
+				TTLFocusBar:SetFormattedText("%1ds", seconds)
+				TTLTargetBar:SetFormattedText("%1ds", seconds)
+			end
+			
+		end
+		
+	end
+end)
+
+local function clearHistory(event)
+	local hp = UnitHealth("target")
+
+	previousHealthTicks = queue.new();
+	for i=1,5 do
+		previousHealthTicks:push(hp)
+	end
+end
+
+local target = CreateFrame("Frame", name, TargetFrameHealthBar)
+target:SetPoint("LEFT", TargetFrameHealthBar, "LEFT", -51, -12)
+target:SetWidth(50)
+target:SetHeight(20)
+target:EnableMouse(true)
+target:RegisterForDrag("LeftButton")
+target:SetClampedToScreen(true)
+target:SetMovable(true)
+target:SetScript("OnDragStart", startDrag)
+target:SetScript("OnDragStop", stopDrag)
+target.unit = "target"
+TTLTargetBar = target:CreateFontString(nil, nil, "TextStatusBarText")
+TTLTargetBar:SetAllPoints(target)
+TTLTargetBar:SetJustifyH("RIGHT")
+
+local target2 = CreateFrame("Frame", name, TargetFrameHealthBar)
+target2:RegisterEvent("PLAYER_TARGET_CHANGED")
+target2:SetScript("OnEvent", clearHistory)
+
+local focus = CreateFrame("Frame", "FocusPercent", FocusFrameHealthBar)
+focus:SetPoint("LEFT", FocusFrameHealthBar, "LEFT", -51, -12)
+focus:SetWidth(50)
+focus:SetHeight(20)
+focus:EnableMouse(true)
+focus:RegisterForDrag("LeftButton")
+focus:SetClampedToScreen(true)
+focus:SetMovable(true)
+focus:SetScript("OnDragStart", startDrag)
+focus:SetScript("OnDragStop", stopDrag)
+focus.unit = "focus"
+TTLFocusBar = focus:CreateFontString(nil, nil, "TextStatusBarText")
+TTLFocusBar:SetAllPoints(focus)
+TTLFocusBar:SetJustifyH("RIGHT")
